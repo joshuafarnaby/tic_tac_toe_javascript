650217c2c27164683aeb02cec1db87ab8e4c48dd
Joshua Farnaby <joshuafarnabyuk@gmail.com>
2021-12-14 11:36:23 +0000

    create gameRules obj

--- script2.js
+++ script2.js
@@ -25,3 +25,30 @@ const gameboard = (function () {
   }
 })();
 
+const gameRules = (function () {
+  const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  const winningMove = (gameboard, token) => {
+    return WINNING_COMBINATIONS.some(combination => {
+      return combination.every(position => {
+        return gameboard[position] == token;
+      })
+    })
+  }
+
+  const positionFree = (gameboard, position) => !gameboard[position];
+
+  return {
+    winningMove,
+    positionFree
+  }
+})()
